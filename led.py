05f9795d6cfa48841290f446037610d83b74b166
pyu <754158521@qq.com>
2024-12-01 17:09:07 +0800

    flash led

---
+++ led.py
@@ -0,0 +1,31 @@
+import RPi.GPIO as GPIO
+import time
+
+led_pin = 11
+
+def setup():
+	GPIO.setmode(GPIO.BOARD)
+	GPIO.setup(led_pin, GPIO.OUT)
+	GPIO.output(led_pin, GPIO.HIGH)
+
+def loop():
+	while True:
+		print ('... led on')
+		GPIO.output(led_pin,GPIO.LOW)
+		time.sleep(0.5)
+		print ('... led off')
+		GPIO.output(led_pin,GPIO.HIGH)
+		time.sleep(0.5)
+		
+def destroy():
+	GPIO.output(led.pin,GPIO.HIGH)
+	GPIO.cleanup()
+	
+if __name__ == '__main__':
+	setup()
+	
+	try:
+		loop()
+	except KeyboardInterrupt:
+		destroy()
+
